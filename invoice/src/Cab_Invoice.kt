d72e5ae6b2582de2a9e61123bc0e5b91442ae928
Tarun Samanta <tarunsamanta@Apples-MacBook-Pro-2.local>
2022-05-24 15:17:00 +0530

    dhyeuvg

--- invoice/src/Cab_Invoice.kt
+++ invoice/src/Cab_Invoice.kt
@@ -4,8 +4,10 @@ class Cab_Invoice {
         const val D_rate=10.0;
         const val T_rate=1.0;
     }
-    fun calculateFair_Multi(Rides: List<Ride>): Double {
-        return Rides.sumOf { it?.distance * D_rate + it?.time * T_rate}
+    fun calculateFair_Multi(Rides: List<Ride>):Array<Double> {
+        val res=Rides.sumOf { it?.distance * D_rate + it?.time * T_rate}
+
+        return  arrayOf(res ,Rides.size.toDouble(), res/Rides.size.toDouble())
     }
     fun calculateFair_Single(distance:Double, time:Double): Double {
         return distance * D_rate + time * T_rate
--- invoice/test/Cab_Test.kt
+++ invoice/test/Cab_Test.kt
@@ -1,8 +1,11 @@
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.doubles.shouldBeExactly
+import org.junit.jupiter.api.Assertions
 
 class Cab_Test : StringSpec() {
     init {
+
+
         "total distance travel is 1.1 kms and total time taken 0.1 units" {
             Cab_Invoice().calculateFair_Single(1.1,0.1) shouldBeExactly 11.1
         }
@@ -15,12 +18,19 @@ class Cab_Test : StringSpec() {
            // val rideOne = Ride(10.0, 1.0)
             Cab_Invoice().calculateFair_Multi(listOf()) shouldBeExactly 0.0
         }
+
+
+
+
         "total distance travel is 7 km and total time taken 1 units" {
-            val rideOne = Ride(3.0, 0.3)
-            val rideTwo = Ride(2.0, 0.2)
-            val rideThree  = Ride(2.0, 0.5)
-            Cab_Invoice().calculateFair_Multi(listOf(rideOne, rideTwo, rideThree)) shouldBeExactly  71.0
+            val rideOne = Ride(3.0, 0.0)
+            val rideTwo = Ride(2.0, 0.0)
+            val rideThree=Ride(5.0,4.0)
+            val a=Cab_Invoice().calculateFair_Multi(listOf(rideOne, rideTwo,rideThree));
+            val b=arrayOf(104.0,3.0,34.666666666666664)
+            Assertions.assertTrue(a contentEquals b)
         }
+
         "total distance travel is 1 km and total time taken 11 units" {
             val rideOne = Ride(0.0, 0.0)
             val rideTwo = Ride(1.0, 1.0)
@@ -34,5 +44,10 @@ class Cab_Test : StringSpec() {
             val rideThree  = Ride(1.5, 2.0)
             Cab_Invoice().calculateFair_Multi(listOf(rideOne, rideTwo, rideThree)) shouldBeExactly  214.0
         }
+
+
+         
+
+
     }
 }
