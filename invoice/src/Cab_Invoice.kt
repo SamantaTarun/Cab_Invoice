bf110f3f408df01e98129ecfb7dc1dded0e34ce3
Tarun Samanta <tarunsamanta@Apples-MacBook-Pro-2.local>
2022-05-24 11:20:56 +0530

    jadga

--- invoice/src/Cab_Invoice.kt
+++ invoice/src/Cab_Invoice.kt
@@ -5,7 +5,11 @@ class Cab_Invoice {
           const val D_rate=10.0;
           const val T_rate=1.0;
         }
-        fun calculateFair(Rides: List<Ride>): Double {
+
+        fun calculateFair_Single(distance : Double, time: Double): Double{
+            return distance * D_rate + time * T_rate
+        }
+        fun calculateFair_Multi(Rides: List<Ride>): Double {
         return Rides.sumOf { it?.distance * D_rate + it?.time * T_rate}
     }
 
