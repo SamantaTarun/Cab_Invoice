2176e4b1ec8774850432267785dbe228e64939b4
Tarun Samanta <tarunsamanta@Apples-MacBook-Pro-2.local>
2022-05-24 12:32:18 +0530

    eufhewuifg

--- invoice/src/Cab_Invoice.kt
+++ invoice/src/Cab_Invoice.kt
@@ -1,16 +1,15 @@
 
 class Cab_Invoice {
-
     companion object{
-          const val D_rate=10.0;
-          const val T_rate=1.0;
-        }
-
-        fun calculateFair_Single(distance : Double, time: Double): Double{
-            return distance * D_rate + time * T_rate
-        }
-        fun calculateFair_Multi(Rides: List<Ride>): Double {
+        const val D_rate=10.0;
+        const val T_rate=1.0;
+    }
+    fun calculateFair_Multi(Rides: List<Ride>): Double {
         return Rides.sumOf { it?.distance * D_rate + it?.time * T_rate}
     }
+    fun calculateFair_Single(distance:Double, time:Double): Double {
+        return distance * D_rate + time * T_rate
+    }
+
 
 }
--- invoice/src/Ride.kt
+++ invoice/src/Ride.kt
@@ -1,1 +1,12 @@
-data class Ride(val distance: Double, val time: Double)
+data class Ride(val distance: Double, val time: Double){
+
+    companion object{
+        const val D_rate=10.0;
+        const val T_rate=1.0;
+    }
+
+    fun calculateFair_Single(): Double{
+        return distance * D_rate + time * T_rate
+    }
+
+}
